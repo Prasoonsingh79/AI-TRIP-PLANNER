adac9cbe377ce04523609ccc0d8c5dddbc1dee26
Prasoonsingh79 <sprasoo876@gmail.com>
2026-04-03 14:37:23 +0530

    commit change

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,8 @@ from dotenv import load_dotenv
 from typing import List
 
 # Internal imports
-import models
-import schemas
-import auth
-import database
-from database import engine, get_db
+from backend import models, schemas, auth, database
+from backend.database import engine, get_db
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
